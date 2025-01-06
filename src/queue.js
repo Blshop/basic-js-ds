6f37ac5e437c3cb70141e72d22843c522db2bb70
Blshop <envworking@gmail.com>
2025-01-06 10:14:24 +0300

    feat queue task solved

--- src/queue.js
+++ src/queue.js
@@ -1,6 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { ListNode } = require('../extensions/list-node.js');
+const { ListNode } = require('../extensions/list-node.js');
 
 /**
  * Implement the Queue with a given interface via linked list (use ListNode extension above).
@@ -14,19 +14,32 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
 class Queue {
-
+  constructor() {
+    this.head
+    this.tail
+  }
   getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
+    return this.head
     // remove line with error and write your code here
   }
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
+  enqueue(value) {
+    if (this.head === undefined) {
+      this.head = new ListNode(value)
+      this.tail = this.head
+    }
+    else {
+      let new_node = new ListNode(value)
+      this.tail.next = new_node
+      this.tail = new_node
+    }
     // remove line with error and write your code here
   }
 
   dequeue() {
-    throw new NotImplementedError('Not implemented');
+    let node_list = this.head
+    this.head = this.head.next
+    return node_list.value
     // remove line with error and write your code here
   }
 }
